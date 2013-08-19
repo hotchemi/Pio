f51770f0397c26532e20c5e20d3bf0a705e0db7e
hotchemi <hot.chemistry21@gmail.com>
2013-08-20 01:54:34 +0900

    delete config.js

--- js/config/oauthInfo.js
+++
@@ -1,7 +0,0 @@
-var oauthInfo = {
-    consumerKey: "IPFARzl7ZBZ5QG50uUuS3g",
-    consumerSecret: "owG8M8PptndcYgqn0o2jxBOi0BwZnaFjLvEfEWgNe7Y",
-    requestTokenUrl: "https://api.twitter.com/oauth/request_token",
-    authorizationUrl: "https://api.twitter.com/oauth/authorize",
-    accessTokenUrl: "https://api.twitter.com/oauth/access_token"
-};
